31906853ab6a36869723f485dc23e129ed6d1e91
Marcelo Antonio <maxyxyxy@gmail.com>
2021-03-28 10:21:27 -0300

    :tada: First commit SNEC

---
+++ app/Models/Message.js
@@ -0,0 +1,13 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Message extends Model {
+
+  students() {
+    return this.belongsToMany('App/Models/Student').pivotModel('App/Models/MessageStudent');
+  }
+}
+
+module.exports = Message
---
+++ start/routes.js
@@ -0,0 +1,10 @@
+'use strict'
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+
+Route.group(() => {
+  Route.resource("/user", "UserController");
+}).middleware("auth");
+
+Route.post("/session", "SessionController.store");
